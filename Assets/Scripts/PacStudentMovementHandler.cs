38bf38b63250757e5721b3b6245f195351b4a898
zake-dev <floatim00@gmail.com>
2023-10-06 17:39:28 +1100

    feat: animate PacStudent according to its direction

--- Assets/Scripts/PacStudentMovementHandler.cs
+++ Assets/Scripts/PacStudentMovementHandler.cs
@@ -12,8 +12,9 @@ public class PacStudentMovementHandler : MonoBehaviour
     {
         soundEffectSource = gameObject.GetComponent<AudioSource>();
         animator = gameObject.GetComponent<Animator>();
-        transform.position = new Vector3(-13.0f, 16.0f, 0.0f);
-        StartCoroutine(moveClockwiseAroundTopLeftBlock());
+
+        float moveSpeed = 0.3f; // move at 0.3s per single grid speed
+        StartCoroutine(moveClockwiseAroundTopLeftBlock(moveSpeed));
     }
 
     void Update()
@@ -21,10 +22,8 @@ public class PacStudentMovementHandler : MonoBehaviour
         if (positionTween != null) positionTween.animate(Time.deltaTime);
     }
 
-    private IEnumerator moveClockwiseAroundTopLeftBlock()
+    private IEnumerator moveClockwiseAroundTopLeftBlock(float moveSpeed)
     {
-        float moveSpeed = 0.3f; // move at 0.3s per single grid speed
-
         Vector3 topLeftCornerPosition = new Vector3(-13.0f, 16.0f, 0.0f);
         Vector3 topRightCornerPosition = new Vector3(-8.0f, 16.0f, 0.0f);
         Vector3 bottomLeftCornerPosition = new Vector3(-13.0f, 12.0f, 0.0f);
@@ -34,18 +33,22 @@ public class PacStudentMovementHandler : MonoBehaviour
         {
             float duration = 5 * moveSpeed;
             positionTween = new PositionTween(transform, topLeftCornerPosition, topRightCornerPosition, Time.time, duration);
+            animator.SetTrigger("moveRight");
             yield return new WaitForSeconds(duration);
 
             duration = 4 * moveSpeed;
             positionTween = new PositionTween(transform, topRightCornerPosition, bottomRightCornerPosition, Time.time, duration);
+            animator.SetTrigger("moveDown");
             yield return new WaitForSeconds(duration);
 
             duration = 5 * moveSpeed;
             positionTween = new PositionTween(transform, bottomRightCornerPosition, bottomLeftCornerPosition, Time.time, duration);
+            animator.SetTrigger("moveLeft");
             yield return new WaitForSeconds(duration);
 
             duration = 4 * moveSpeed;
             positionTween = new PositionTween(transform, bottomLeftCornerPosition, topLeftCornerPosition, Time.time, duration);
+            animator.SetTrigger("moveUp");
             yield return new WaitForSeconds(duration);
         }
     }
